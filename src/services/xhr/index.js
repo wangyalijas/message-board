516abbcb10c316f184a4debcff16f7b469f6e7b4
wangyalijas <1114920411@qq.com>
2017-10-31 01:10:47 +0800

    Add adapter to xhr

--- src/services/xhr/index.js
+++ src/services/xhr/index.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
+import mock from '@/mock/index'
+import settle from 'axios/lib/core/settle'
+import httpCodeService from '../httpCodeService'
+
 
 class Service {
   constructor() {
@@ -6,7 +10,24 @@ class Service {
       headers: {csrf: 'token'}
     });
     service.interceptors.response.use(this.handleSuccess, this.handleError);
+    service.interceptors.request.use(this.handleRequest);
     this.service = service;
+  }
+
+  handleRequest(config) {
+    config.adapter = config => {
+      return new Promise((resolve, reject) => {
+        let data = mock(config.url, config.method);
+        let response = {
+          config: config,
+          status: data.header.status,
+          statusText: httpCodeService.get(data.header.status)
+        };
+
+        settle(resolve, reject, Object.assign(response, data));
+      });
+    };
+    return config;
   }
 
   handleSuccess(response) {
@@ -16,44 +37,54 @@ class Service {
   handleError = (error) => {
     switch (error.response.status) {
       case 401:
-        this.redirectTo(document, '/')
+        this.redirectTo(document, '/');
         break;
       case 404:
-        this.redirectTo(document, '/404')
+        this.redirectTo(document, '/404');
         break;
       default:
-        this.redirectTo(document, '/500')
+        this.redirectTo(document, '/500');
         break;
     }
     return Promise.reject(error)
-  }
+  };
 
   redirectTo = (document, path) => {
     document.location = path
-  }
+  };
 
   get(path, callback) {
-    return this.service.get(path).then(
-      (response) => callback(response.status, response.data)
-    );
+    let request = this.service.get(path);
+    if (typeof callback === 'function') {
+      return request.then();
+    }
+    return request;
   }
 
   patch(path, payload, callback) {
-    return this.service.request({
+    let request = this.service.request({
       method: 'PATCH',
       url: path,
       responseType: 'json',
       data: payload
-    }).then((response) => callback(response.status, response.data));
+    });
+    if (typeof callback === 'function') {
+      return request.then((response) => callback(response.status, response.data));
+    }
+    return request;
   }
 
   post(path, payload, callback) {
-    return this.service.request({
+    let request = this.service.request({
       method: 'POST',
       url: path,
       responseType: 'json',
       data: payload
-    }).then((response) => callback(response.status, response.data));
+    });
+    if (typeof callback === 'function') {
+      return request.then((response) => callback(response.status, response.data));
+    }
+    return request
   }
 }
 
